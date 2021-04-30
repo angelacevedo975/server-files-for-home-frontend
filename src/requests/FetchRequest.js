f24237c21a103e34003d8d329d4e5497810144f9
Angel Acevedo <angelacevedo975@gmail.com>
2021-04-30 13:35:01 -0500

    change endpoint

--- src/requests/FetchRequest.js
+++ src/requests/FetchRequest.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "http://localhost:3005" 
+export const BASE_API_URL = "https://server-files-back.herokuapp.com" 
 
 const fetchRequest = async (requestPath, requestParams = {}, headers = {}) => {
 	try {
